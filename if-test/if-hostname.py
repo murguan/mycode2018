10a41ed887cf4d51daa6ccf28a8b3bbc6c8cb55a
murguan <amurguia64@gmail.com>
2018-11-05 18:43:33 -0500

    my latest commit

---
+++ if-test/if-hostname.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# Lab13 - testing with if
+# hostname = 'MTG'
+hostname = input("what is the host name?")
+if hostname.lower() == 'mtg':
+    print('The hostname was found to be mtg')
+#    user_input = input('Please enter mtg as you wish:')
+    print("You told me:" + hostname)
+# elif user_input == 'MTG', 'mTg', 'MTg', 'mTG', 'MtG'
+#    print(user_input)
+print('Exiting the script')
+    
